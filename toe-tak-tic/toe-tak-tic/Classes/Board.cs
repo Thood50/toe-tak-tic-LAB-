99f5562ec5fdbb0c467c65afa07c6a41fa6b0790
Tyler Hood <tyler.r.hood@gmail.com>
2019-01-13 15:37:32 -0800

    I completely forgot to do commits through out, this project was hard and I loved it getting my mind around what I think was intended functions without adding methods myself! Got caught up in all that and didnt do any incromenting commits sorry!

---
+++ toe-tak-tic/toe-tak-tic/Classes/Board.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab04_TicTacToe.Classes
+{
+    class Board
+    {
+		/// <summary>
+		/// Tic Tac Toe Gameboard states
+		/// </summary>
+		public string[,] GameBoard = new string[,]
+		{
+			{"1", "2", "3"},
+			{"4", "5", "6"},
+			{"7", "8", "9"},
+		};
+
+
+		public void DisplayBoard()
+		{
+
+            //TODO: Output the board to the console
+            int rows = GameBoard.GetLength(0);
+            int cols = GameBoard.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int k = 0; k < cols; k++)
+                {
+                    if (k == 2)
+                    {
+                        Console.WriteLine($" {GameBoard[i, k]} ");
+                    }
+                    if (k < 2)
+                    {
+                        Console.Write($" {GameBoard[i, k]} |");
+                    }
+                }
+            }
+
+        }
+	}
+}
---
+++ toe-tak-tic/toe-tak-tic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Lab04_TicTacToe.Classes;
+
+namespace toe_tak_tic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Welcome to toe-tak-tic");
+            Console.WriteLine("Who would like to be player One?");
+            Player playerOne = new Player();
+            playerOne.Name = Console.ReadLine();
+            Console.WriteLine($"Who wants to play {playerOne.Name}");
+            Player playerTwo = new Player();
+            playerTwo.Name = Console.ReadLine();
+            Game newGame = new Game(playerOne, playerTwo);
+            Player winner = newGame.Play();
+            if (winner.Name == "Its a draw!")
+            {
+                Console.WriteLine($"Sorry, {winner.Name}");
+                Console.ReadLine();
+            }
+            Console.WriteLine($"Congrats {newGame.Play().Name}! You WON!");
+            Console.ReadLine();
+        }
+    }
+}
